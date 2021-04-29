0f428358aeade3184215f69728831a8c63efa846
unknown <jean-bateee@hotmail.fr>
2021-04-29 14:57:34 +0200

    add slide

---
+++ asset/script2.js
@@ -0,0 +1,10 @@
+const price = document.getElementById("priceNumber")
+
+document.getElementById("myRange").oninput = function() {
+    var value = (this.value-this.min)/(this.max-this.min)*100
+    this.style.background = 'linear-gradient(to right, hsl(174, 77%, 80%) 0%, hsl(174, 77%, 80%)' + value + '%, hsl(224, 65%, 95%) ' + value + '%, hsl(224, 65%, 95%) 100%)'
+    price.innerHTML = "$" + this.value + ".00"
+};
+
+
+
